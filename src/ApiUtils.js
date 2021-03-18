c1889c2cce2bfb73dd93bd4d2ee5d41eb8db6a66
Adrien Rochedy <adrien.rochedy@gmail.com>
2021-03-18 08:19:10 +0100

    remove accent

--- src/ApiUtils.js
+++ src/ApiUtils.js
@@ -15,6 +15,18 @@ var ApiUtils = {
   {
     return pushNotificationInstance;
   },
+  removeAccents(str) {
+    let accents = 'ÀÁÂÃÄÅàáâãäåßÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+    let accentsOut = "AAAAAAaaaaaaBOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+    str = str.split('');
+    str.forEach((letter, index) => {
+      let i = accents.indexOf(letter);
+      if (i != -1) {
+        str[index] = accentsOut[i];
+      }
+    })
+    return str.join('');
+  },
   ISDEBUG() {
     return ISDEBUG;
   },
