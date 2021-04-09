4c939067bd75c29f92265ade5ac0ae72b3a406d7
Adrien Rochedy <adrien.rochedy@gmail.com>
2021-04-09 16:40:41 +0200

    fix sponsors logo

--- src/home/Sponsors.tsx
+++ src/home/Sponsors.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Image, View} from 'react-native';
+import {Dimensions, Image, View} from 'react-native';
 
 import {TemplateArrayImagesSponsorPath} from './../globalsModifs';
 
@@ -10,30 +10,41 @@ export class Sponsors extends Component {
     this.state = {};
   }
 
-  componentDidMount() {}
+  componentDidMount() {
 
   render() {
     return (
       <View>
         <View
           style={{
+            borderBottomWidth: 1,
+            borderBottomColor: '#DDDDDD',
+            width: Dimensions.get('screen').width - 30,
+            marginLeft: 15,
+            marginRight: 10,
+            marginTop: 0,
+          }}></View>
+        <View
+          style={{
             display: 'flex',
             flexDirection: 'row',
-            justifyContent: 'space-around',
+            justifyContent: 'space-between',
             marginTop: 10,
             width: '100%',
             paddingHorizontal: 10,
             marginBottom: 20,
           }}>
-          {TemplateArrayImagesSponsorPath.map((sponsor) => {
-            <View style={{width: '25%', height: 50}}>
-              <Image
-                source={sponsor}
-                resizeMethod="resize"
-                resizeMode="contain"
-                style={{height: '100%', width: '100%'}}
-              />
-            </View>;
+          {TemplateArrayImagesSponsorPath.map((sponsor, index) => {
+            return (
+              <View style={{width: '25%', height: 50}}>
+                <Image
+                  source={sponsor}
+                  resizeMethod="resize"
+                  resizeMode="contain"
+                  style={{height: '100%', width: '100%'}}
+                />
+              </View>
+            );
           })}
         </View>
       </View>
